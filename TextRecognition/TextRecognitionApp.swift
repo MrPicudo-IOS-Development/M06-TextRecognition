08a26d5bd6dc74c7c05ab496478c062178dbee99
Miguel Torres <420093743@pcpuma.acatlan.unam.mx>
2023-04-27 16:26:23 -0600

    Proyecto de práctica

---
+++ TextRecognition/TextRecognitionApp.swift
@@ -0,0 +1,12 @@
+/* TextRecognitionApp.swift --> TextRecognition. Created by Miguel Torres on 24/04/23. */
+
+import SwiftUI
+
+@main
+struct TextRecognitionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
